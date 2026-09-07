22ff354d2954583d5d281e71af2b67df6c135ff9
Luiz jr <Luiz.apj07@aluno.ifsc.edu.br>
2026-09-07 14:46:01 -0300

    Ex2 feito

--- exercicio 10.2.sql
+++ exercicio 10.2.sql
@@ -43,4 +43,40 @@ UPDATE `projEmp`
 SET `hrTrab` = `hrTrab` + 15
 WHERE `codProj` = '3'
 
+-- 8
 
+DELETE FROM `projEmp`
+WHERE `codProj` = '3';
+
+DELETE FROM `projeto`
+WHERE `preco` < '6000';
+
+-- 9
+
+UPDATE `projeto`
+SET `dtEstimada` = DATE_ADD(`dtEstimada`, INTERVAL 7 DAY)
+WHERE `cpfGerente` = '444.444.444-44';
+
+-- 10
+
+INSERT INTO `empregado`(`cpf`, `nome`, `cargo`)
+VALUES('999.999.999-99', 'Schalata', 'Programador');
+
+DELETE FROM `empregado`
+WHERE `cpf` = '999.999.999-99';
+
+-- 11
+
+UPDATE `empregado`
+SET `cargo` = 'Gerente de Projetos'
+WHERE `cpf` = '333.333.333-33';
+
+UPDATE `projeto`
+SET `cpfGerente` = '333.333.333-33'
+WHERE `cpfCliente` = '222.222.222-22';
+
+-- 12
+
+DELETE FROM `projeto`
+WHERE `dtFim` < CURDATE()
+AND `cpfCliente` <> '111.111.111-11';
